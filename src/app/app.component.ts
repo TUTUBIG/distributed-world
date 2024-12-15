e6684d45f1c4840e02f6cfaf0f976f804e901b0b
Alvin <liukui@tomo.inc>
2024-12-15 18:38:34 +0800

    static home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'website';
+  title = 'DisWorld';
 }
