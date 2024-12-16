153963d284ff21990853884258601a2b5814d199
Alvin <liukui@tomo.inc>
2024-12-17 02:18:02 +0800

    page router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {GameLibraryComponent} from './game-library/game-library.component';
+import {HistoryComponent} from './history/history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'games',component: GameLibraryComponent },
+  { path: 'history', component: HistoryComponent },
+];
+
