85d2fa0265f21a479004631185b4622fc7a6126a
Alvin <liukui@tomo.inc>
2024-12-18 23:29:55 +0800

    connect to metamask wallet

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
+import {WalletService} from '../../web3/wallet.service';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +12,15 @@ import {NgOptimizedImage} from '@angular/common';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  walletConnectStatus = false
+  wallet = new WalletService();
 
+  async loginWithEthereum() {
+    const signature = await this.wallet.web3LoginViaMetamask()
+    if (!signature) {
+      console.log('signature is missing');
+    }
+    console.log(signature);
+    this.walletConnectStatus = true;
+  }
 }
